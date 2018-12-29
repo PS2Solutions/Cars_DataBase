9dc8dcb89a7743c2b22a65d3c8ad024ea7aaf69a
PS2Solutions <ps2solutions2018@gmail.com>
2018-12-29 19:21:15 +0530

    Added ReportGetQuotDetailsByDate.sql

---
+++ DML/ReportGetQuotDetailsByDate.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE PROCEDURE `ReportGetQuotDetailsByDate`(IN `dateFrom` datetime,IN `dateTo` datetime) 
+BEGIN
+SELECT CTPE.Title, CTPE.ReferenceNo as 'Reference No',CTPE.CreatedDate as 'Created Date',CTPE.Amount, 
+CTPE.Status, CTPE.Name AS 'Customer Name',CTPE.Type AS 'Quotation Type', IFNULL(CNTR.ID,'Not yet Contract') as Contract
+ from (
+(SELECT QUOT.ContractID, QUOT.Title as 'Title',QUOT.ReferenceNo, QUOT.CreatedDate, QUOT.Amount, 
+QUOT.Status, CUST.Name,QTYP.Type FROM `quotations` QUOT 
+join customers CUST on CUST.ID = QUOT.CustomerID Join quotationtypes QTYP on QUOT.QuotationTypeID=QTYP.ID)) as CTPE
+Left JOIN contracts CNTR on CNTR.ID = CTPE.ContractID
+WHERE (DATE(CTPE.CreatedDate) BETWEEN dateFrom AND dateTo);
+ END$$
+DELIMITER ;
