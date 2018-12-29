ea24447ab625fea61ecf87805cc6d6c2078b027f
PS2Solutions <ps2solutions2018@gmail.com>
2018-12-30 04:32:37 +0530

    Chnages for Contract

--- DML/UpdateContractDetails.sql
+++ DML/UpdateContractDetails.sql
@@ -1,21 +1,20 @@
 DELIMITER $$
 CREATE PROCEDURE `UpdateContractDetails`(IN `ContractID` INT(10), IN `ReferenceNo` VARCHAR(20), IN `StartDate` datetime, 
-IN `EndDate` datetime, IN `CollectedAmount` decimal(10,2), IN `LastCollectionDate` datetime) 
+IN `EndDate` datetime, IN `CollectedAmount` decimal(10,2), IN `LastCollectionDate` datetime, IN `AgrementReference` VARCHAR(20), OUT `Response` VARCHAR(100)) 
  BEGIN
 	UPDATE 
         contracts 
     SET
-        StartDate = StartDate
-        ,EndDate = EndDate
-        ,CollectedAmount = CollectedAmount
-		,LastCollectionDate = LastCollectionDate
+	CollectedAmount = CollectedAmount,
+	LastCollectionDate = LastCollectionDate
      WHERE
         ID = ContractID;
         
     IF NOT EXISTS (SELECT 1 FROM contracts WHERE ID = ContractID) THEN 
 		INSERT INTO
-			contracts(ReferenceNo, StartDate, EndDate, CollectedAmount, LastCollectionDate)
-		VALUES(ReferenceNo, StartDate, EndDate, CollectedAmount, LastCollectionDate);
+			contracts(ReferenceNo, StartDate, EndDate, CollectedAmount, LastCollectionDate, AgrementReference)
+		VALUES(ReferenceNo, StartDate, EndDate, CollectedAmount, LastCollectionDate, AgrementReference);
 	END IF;	
+	SET Response = 'Success';
  END$$
 DELIMITER ;
