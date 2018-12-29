732be7973a9e666faf2af860d777afe8b4a9ce6d
PS2Solutions <ps2solutions2018@gmail.com>
2018-12-29 16:30:53 +0530

    Issue fix GetContractLastMonthReport

--- DML/GetContractLastMonthReport.sql
+++ DML/GetContractLastMonthReport.sql
@@ -2,20 +2,21 @@ DELIMITER $$
 CREATE PROCEDURE `GetContractLastMonthReport`()
  BEGIN
 SELECT 
-CNTT.ReferenceNo AS 'Reference No',CNTT.StartDate as 'Start Date',SUM(AMT.LaborAmount)+ SUM(AMT.PurchaseAmount) as 'Amount', CNTT.CollectedAmount as 'Collected Amount'
+CNTT.ReferenceNo AS 'Reference No',CNTT.StartDate as 'Start Date',SUM(IFNULL(AMT.LaborAmount,0))+ SUM(IFNULL(AMT.PurchaseAmount,0)) as 'Amount', CNTT.CollectedAmount	 as 'Collected Amount'
 FROM 
 (SELECT cnt.ID,cnt.ReferenceNo, cnt.StartDate,cnt.CollectedAmount
 FROM 
-contracts cnt where cnt.StartDate >= Date(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND cnt.EndDate is NULL)CNTT JOIN
-(SELECT LBC.CID,LBC.LaborAmount,EPDT.PurchaseAmount FROM (SELECT 
+contracts cnt where cnt.StartDate >= Date(DATE_SUB(NOW(), INTERVAL 1 MONTH)) AND cnt.EndDate is NULL)CNTT 
+LEFT JOIN
+(SELECT LBC.CID,LBC.LaborAmount,IFNULL(EPDT.PurchaseAmount,0) as PurchaseAmount FROM (SELECT 
 clc.ContractID AS CID
-,SUM(clc.Wage)+SUM(clc.TA)+SUM(clc.FA)+SUM(clc.OverTime) AS LaborAmount
+,SUM(IFNULL(clc.Wage,0))+SUM(IFNULL(clc.TA,0))+SUM(IFNULL(clc.FA,0))+SUM(IFNULL(clc.OverTime,0)) AS LaborAmount
 FROM 
 contractlaborchargedetails clc
-GROUP BY clc.ContractID)LBC JOIN
+GROUP BY clc.ContractID)LBC LEFT JOIN
 (SELECT 
 epd.ContractID
-,SUM(epd.Amount) AS PurchaseAmount
+,SUM(IFNULL(epd.Amount,0)) AS PurchaseAmount
 FROM 
 extrapurchasedetails epd
 GROUP BY epd.ContractID) EPDT ON LBC.CID = EPDT.ContractID ) AMT ON CNTT.ID = AMT.CID;
