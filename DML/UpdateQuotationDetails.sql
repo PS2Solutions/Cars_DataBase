3f730c85a8f52f7e59d818039179a618efb5d5f5
PS2Solutions <ps2solutions2018@gmail.com>
2018-12-29 22:11:32 +0530

    Chnages for Quotations

--- DML/UpdateQuotationDetails.sql
+++ DML/UpdateQuotationDetails.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE `UpdateQuotationDetails`(IN `QuoteID` INT(11), IN `Title` VARCHAR(100), IN `ReferenceNo` VARCHAR(100), 
 IN `CustomerID` INT(10), IN `Amount` decimal(10,3), IN `Status` tinyint(1),
- IN `Address1` VARCHAR(100), IN `Address2` VARCHAR(100),IN `QuotationTypeID` INT(10), IN `ContractID` INT(10), `LaborCharge`  decimal(10,3), OUT `Response` INT) 
+ IN `Address1` VARCHAR(100), IN `Address2` VARCHAR(100),IN `QuotationTypeID` INT(10), IN `ContractID` INT(10), IN `LaborCharge`  decimal(10,3), IN `CreatedDate` Datetime, OUT `Response` INT) 
  BEGIN
 	UPDATE 
         quotations 
@@ -24,7 +24,7 @@ IN `CustomerID` INT(10), IN `Amount` decimal(10,3), IN `Status` tinyint(1),
     IF NOT EXISTS (SELECT 1 FROM quotations WHERE ID = QuoteID) THEN 
 		INSERT INTO
 			quotations(Title, ReferenceNo, CustomerID, CreatedDate, Amount, Status, Address1, Address2, QuotationTypeID, ContractID, LaborCharge)
-		VALUES(Title, ReferenceNo, CustomerID, Now(), Amount, Status, Address1, Address2, QuotationTypeID, ContractID, LaborCharge);
+		VALUES(Title, ReferenceNo, CustomerID, CreatedDate, Amount, Status, Address1, Address2, QuotationTypeID, ContractID, LaborCharge);
         SET Response = LAST_INSERT_ID();
 	END IF;	
  END$$
