6b5be00e6a70c63cb3f8b899d74ae34571a12180
ps2 <ps2solutions2018@gmail.com>
2019-03-10 12:32:44 +0530

    SP modification

--- DML/ReportGetLabourDetailsByContract.sql
+++ DML/ReportGetLabourDetailsByContract.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
-CREATE PROCEDURE `ReportGetLabourDetailsByContract`(IN `dateFrom` datetime,IN `dateTo` datetime, IN `CntrctID` int) 
+CREATE PROCEDURE `ReportGetLabourDetailsByContract`(IN `DateFrom` datetime,IN `DateTo` datetime, IN `ContractID` int,IN `QuoteID` int) 
 BEGIN
 SELECT 
 	clc.LaborID AS LID, LBRS.Name as Name
 	,SUM(IFNULL(clc.Wage,0))+SUM(IFNULL(clc.TA,0))+SUM(IFNULL(clc.FA,0))+SUM(IFNULL(clc.OverTime,0)) AS LaborAmount
 	FROM  
-	contractlaborchargedetails clc Inner Join labors LBRS on LBRS.ID = CLC.LaborID WHERE  clc.ContractID = CntrctID AND 
+	contractlaborchargedetails clc Inner Join labors LBRS on LBRS.ID = CLC.LaborID WHERE  clc.ContractID = ContractID AND 
 	(DATE(clc.Date) BETWEEN dateFrom AND dateTo)
 	GROUP By clc.LaborID;
  END$$
