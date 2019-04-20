19d075454489dab7a210da0e3441f2507f5830b8
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 14:51:59 +0530

    Changed report details

--- DML/V3/GetContractDetails.sql
+++ DML/V3/GetContractDetails.sql
@@ -8,12 +8,13 @@ Select CNTR.ID,
 	CNTR.ReferenceNo As 'Reference Number',
 	Date_Format(CNTR.StartDate,'%d-%M-%Y') As 'Start Date',
 	CNTR.AgrementReference As 'Agreement Reference', 
-	IFNULL(CNPY.Amount,0) as 'Collected Amount',
+	Sum(IFNULL(CNPY.Amount,0)) as 'Collected Amount',
 	CNPY.Date As 'Collected Date'
 	From contracts as CNTR
 	left join contractpayments CNPY on CNPY.ContractID = CNTR.ID
 	WHERE  CNTR.EndDate is NULL 
-	order by CNPY.Date DESC;
+    	Group by CNTR.ID
+	order by CNPY.Date DESC
 	
   END$$
 DELIMITER ;
--- DML/V3/InsertReports.sql
+++ DML/V3/InsertReports.sql
@@ -1,11 +1,11 @@
 Delete from reports;
 INSERT INTO `reports` ( `Name`, `SPName`, `IsFilterAvailable`, `ReportType`) VALUES
-('GetContractLastMonthReport ', 'GetContractLastMonthReport', NULL, 1),
-('GetLabourDetailsByDate', 'GetLabourDetailsByDate ', 1, 2),
+('Contract Last Month Details ', 'GetContractLastMonthReport', NULL, 1),
+('Labour Details By Date', 'GetLabourDetailsByDate ', 1, 2),
 ('Quotation Details', 'ReportGetQuotDetailsByDate', 1, 0),
-('ReportGetLabourDetailsByContract', 'ReportGetLabourDetailsByContract', 1, 1),
-('ReportLabourPayslip', 'ReportLabourPayslip', 1, 3),
-('GetLabourDetailsByDate','GetLabourDetailsByDate',1,2),
-('ReportGetLabourDetailsByContract','ReportGetLabourDetailsByContract',1,1),
-('ReportGetPaymountByContract','ReportGetPaymountByContract',1,0),
-('LaborWageReport','LaborWageReport',1,1);
+('Labour Details By Contract', 'ReportGetLabourDetailsByContract', 1, 1),
+('Labour Payslip', 'ReportLabourPayslip', 1, 3),
+('Labour Details By Date','GetLabourDetailsByDate',1,2),
+('Labour Details By Contract','ReportGetLabourDetailsByContract',1,1),
+('Paymount By Contract','ReportGetPaymountByContract',1,1),
+('Labor Wage Details','LaborWageReport',1,1);
