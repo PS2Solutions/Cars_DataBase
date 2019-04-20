0c76086dd32a9ed4cb4a9ad241e9f7205531a01b
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 18:01:54 +0530

    updated ReportGetLabourDetailsByContract.sql

--- DML/V3/ReportGetLabourDetailsByContract.sql
+++ DML/V3/ReportGetLabourDetailsByContract.sql
@@ -3,25 +3,25 @@ DELIMITER $$
 CREATE PROCEDURE `ReportGetLabourDetailsByContract`(IN `DateFrom` datetime,IN `DateTo` datetime, IN `ContractID` int,IN `QuoteID` int, IN `LaborID` int)
 BEGIN
 SELECT 
-L.LaborID AS 'Labor ID'
-	,L.Name as Name
-	,L.Designation as 'Designation'
+	lacd.Name as Name
+	,lacd.Designation as 'Designation'
 	,IFNULL(lacd.AmountToPay,0)+ IFNULL(epdd.extraPurchase,0) as 'Labor Amount'
 FROM 
-labors L
- LEFT JOIN
  (
      SELECT
-         lac.LaborID
+         lac.LaborID, 
+		 Labor.Name,
+		 Labor.Designation
      ,SUM(IFNULL(lac.Wage,0))+SUM(IFNULL(lac.TA,0))+SUM(IFNULL(lac.FA,0))+SUM(IFNULL(lac.OverTime,0)) AmountToPay
      FROM 
      contractlaborchargedetails lac
+	 inner join Labor on lac.LaborID =  Labor.ID
 	 WHERE  (DATE(lac.Date) BETWEEN dateFrom AND dateTo)
 	 AND  lac.ContractID = ContractID
      GROUP BY 
     lac.LaborID
-     ) lacd ON L.ID = lacd.LaborId
-     LEFT JOIN
+     ) lacd
+     LEFT JOIN 
  (
      SELECT
          epd.LaborID
@@ -32,7 +32,7 @@ labors L
 	 AND  epd.ContractID = ContractID
      GROUP BY 
     epd.LaborID
-     ) epdd ON L.ID = epdd.LaborId;
+     ) epdd ON lacd.LaborID = epdd.LaborId;
 
 	 END$$
 DELIMITER ;
