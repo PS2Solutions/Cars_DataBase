8fac0017fc20fd085d193477d5a832ef70c36f4a
ps2 <ps2solutions2018@gmail.com>
2019-04-20 17:43:19 +0530

    modified

--- DML/V3/ReportLabourPayslip.sql
+++ DML/V3/ReportLabourPayslip.sql
@@ -2,20 +2,41 @@ DROP PROCEDURE IF EXISTS ReportLabourPayslip;
 DELIMITER $$
 CREATE PROCEDURE `ReportLabourPayslip`(IN `DateFrom` datetime,IN `DateTo` datetime, IN `ContractID` int,IN `QuoteID` int, IN `LaborID` int)
 BEGIN
-SELECT LBRSD.AmountPayed as 'Amount Paid',
-LBRSD.AmountToPay as 'Amount to Pay',
-(LBRSD.AmountToPay- LBRSD.AmountPayed) AS 'Payable Amount'
-FROM
+SELECT 
+IFNULL(APD.AmountPayed,0) as 'Amount Paid'
+,IFNULL(lacd.AmountToPay,0)+ IFNULL(epdd.extraPurchase,0) as 'Amount to Pay'
+,IFNULL(lacd.AmountToPay,0)+ IFNULL(epdd.extraPurchase,0)- IFNULL(APD.AmountPayed,0) as 'Payable Amount'
+FROM 
+labors L
+LEFT JOIN
 (SELECT 
-	Sum(IFNULL(LBP.Amount,0)) As AmountPayed
-    ,SUM(IFNULL(lac.Wage,0))+SUM(IFNULL(lac.TA,0))+SUM(IFNULL(lac.FA,0))+SUM(IFNULL(lac.OverTime,0)) + SUM(IFNULL(epd.Amount,0)) As AmountToPay
+     LBP.LaborID as LaborID
+	,SUM(IFNULL(LBP.Amount,0)) As AmountPayed
+    
 FROM  
-	labors LBRS 
-Left Join laborpayments LBP on LBRS.ID = LBP.LaborID
-Left join contractlaborchargedetails lac on lbrs.ID = lac.LaborID
-Left join extrapurchasedetails epd on lbrs.ID = epd.LaborID
-WHERE  LBRS.ID = LaborID
-GROUP by
-	LBRS.ID) as LBRSD;
+	laborpayments LBP
+GROUP BY 
+    LBP.LaborID) AS APD ON L.ID = APD.LaborID
+ LEFT JOIN
+ (
+     SELECT
+         lac.LaborID
+     ,SUM(IFNULL(lac.Wage,0))+SUM(IFNULL(lac.TA,0))+SUM(IFNULL(lac.FA,0))+SUM(IFNULL(lac.OverTime,0)) AmountToPay
+     FROM 
+     contractlaborchargedetails lac
+     GROUP BY 
+    lac.LaborID
+     ) lacd ON L.ID = lacd.LaborId
+     LEFT JOIN
+ (
+     SELECT
+         epd.LaborID
+     ,SUM(IFNULL(epd.Amount,0)) as extraPurchase
+     FROM 
+     extrapurchasedetails epd
+     GROUP BY 
+    epd.LaborID
+     ) epdd ON L.ID = epdd.LaborId
+WHERE  L.ID = LaborID;
 	 END$$
 DELIMITER ;
