b986913b0b5fe93b7ae0b7c59962ae67a8d04574
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 15:10:16 +0530

    Insert reports

--- DML/V3/InsertReports.sql
+++ DML/V3/InsertReports.sql
@@ -7,5 +7,5 @@ INSERT INTO `reports` ( `Name`, `SPName`, `IsFilterAvailable`, `ReportType`) VAL
 ('Labour Payslip', 'ReportLabourPayslip', 1, 3),
 ('Labour Details By Date','GetLabourDetailsByDate',1,2),
 ('Labour Details By Contract','ReportGetLabourDetailsByContract',1,1),
-('Paymount By Contract','ReportGetPaymountByContract',1,1),
+('Contract Details','ReportGetPaymountByContract',1,1),
 ('Labor Wage Details','LaborWageReport',1,1);
