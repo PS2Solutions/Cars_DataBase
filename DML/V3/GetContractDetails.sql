ca03cb287c82696e173308c28a8a4cd0e989f481
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 14:42:23 +0530

    Added GetContractDetails

---
+++ DML/V3/GetContractDetails.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS GetContractDetails;
+DELIMITER $$
+CREATE PROCEDURE `GetContractDetails`()
+BEGIN
+
+
+Select CNTR.ID, 
+	CNTR.ReferenceNo As 'Reference Number',
+	Date_Format(CNTR.StartDate,'%d-%M-%Y') As 'Start Date',
+	CNTR.AgrementReference As 'Agreement Reference', 
+	IFNULL(CNPY.Amount,0) as 'Collected Amount',
+	CNPY.Date As 'Collected Date'
+	From contracts as CNTR
+	left join contractpayments CNPY on CNPY.ContractID = CNTR.ID
+	WHERE  CNTR.EndDate is NULL 
+	order by CNPY.Date DESC;
+	
+  END$$
+DELIMITER ;
+	
