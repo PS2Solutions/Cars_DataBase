6cd11c8dc05f29bb689f757949a1009c624ede4f
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 16:50:59 +0530

    Fixed issue in ReportLabourPayslip.sql

--- DML/V3/InsertReports.sql
+++ DML/V3/InsertReports.sql
@@ -2,7 +2,6 @@ Delete from reports;
 INSERT INTO `reports` ( `Name`, `SPName`, `IsFilterAvailable`, `ReportType`) VALUES
 ('Labour Details By Date', 'GetLabourDetailsByDate ', 1, 0),
 ('Quotation Details', 'ReportGetQuotDetailsByDate', 1, 0),
-('Labour Details By Contract', 'ReportGetLabourDetailsByContract', 1, 1),
 ('Labour Payslip', 'ReportLabourPayslip', 1, 3),
 ('Labour Details By Contract','ReportGetLabourDetailsByContract',1,1),
 ('Contract Details','ReportGetPaymountByContract',1,1),
--- DML/V3/ReportLabourPayslip.sql
+++ DML/V3/ReportLabourPayslip.sql
@@ -2,24 +2,21 @@ DROP PROCEDURE IF EXISTS ReportLabourPayslip;
 DELIMITER $$
 CREATE PROCEDURE `ReportLabourPayslip`(IN `DateFrom` datetime,IN `DateTo` datetime, IN `ContractID` int,IN `QuoteID` int, IN `LaborID` int)
 BEGIN
-SELECT LBPYT.Amount as 'Amount Paid', 
-Date_Format(LBPYT.Date,'%d-%M-%Y') as 'Amount Paid Date',
-	SUM(IFNULL(CLCD.Wage,0))+SUM(IFNULL(CLCD.TA,0))+SUM(IFNULL(CLCD.FA,0))+SUM(IFNULL(CLCD.OverTime,0)) AS 'Payable Amount',
-	(SUM(IFNULL(CLCD.Wage,0))+SUM(IFNULL(CLCD.TA,0))+SUM(IFNULL(CLCD.FA,0))+SUM(IFNULL(CLCD.OverTime,0)) - LBPYT.Amount) as 'Amount To Pay'
-	from
+SELECT LBRSD.AmountPayed as 'Amount Paid',
+LBRSD.AmountToPay as 'Amount to Pay',
+(LBRSD.AmountToPay- LBRSD.AmountPayed) AS 'Payable Amount'
+FROM
 (SELECT 
-	Sum(IFNULL(LBP.Amount,0)) As Amount,
-	LBP.Date, LBRS.ID
-	FROM  
-	labors LBRS Left Join laborpayments LBP on LBRS.ID = LBP.LaborID ) as LBPYT
-	inner join contractlaborchargedetails as CLCD
-	on CLCD.LaborID = LBPYT.ID
-    WHERE  LBPYT.ID = LaborID
-	AND 
-	(DATE(LBPYT.Date) BETWEEN dateFrom AND dateTo)
-	GROUP By LBPYT.ID
-	order by LBPYT.Date Desc;
- END$$
+	Sum(IFNULL(LBP.Amount,0)) As AmountPayed
+    ,SUM(IFNULL(lac.Wage,0))+SUM(IFNULL(lac.TA,0))+SUM(IFNULL(lac.FA,0))+SUM(IFNULL(lac.OverTime,0)) + SUM(IFNULL(epd.Amount,0)) As AmountToPay
+FROM  
+	labors LBRS 
+Left Join laborpayments LBP on LBRS.ID = LBP.LaborID
+Left join contractlaborchargedetails lac on lbrs.ID = lac.LaborID
+Left join extrapurchasedetails epd on lbrs.ID = epd.LaborID
+WHERE  LBRS.ID = LaborID AND 
+(DATE(LBRS.Date) BETWEEN dateFrom AND dateTo)
+GROUP by
+	LBRS.ID) as LBRSD;
+	 END$$
 DELIMITER ;
-
-
