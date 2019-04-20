56caea8d2fc443f738c299abcdcf93005e9b35ff
PS2Solutions <ps2solutions2018@gmail.com>
2019-04-20 15:00:36 +0530

    modified date for ReportGetPaymountByContract.sql

--- DML/V3/ReportGetPaymountByContract.sql
+++ DML/V3/ReportGetPaymountByContract.sql
@@ -8,7 +8,7 @@ BEGIN
 	Date_Format(CNTR.EndDate,'%d-%M-%Y') As 'End Date',
 	CNTR.AgrementReference As 'Agreement Reference', 
 	IFNULL(CNPY.Amount,0) as 'Collected Amount',
-	CNPY.Date As 'Collected Date'
+	Date_Format(CNPY.Date,'%d-%M-%Y') As 'Collected Date'
 	From contracts as CNTR
 	left join contractpayments CNPY on CNPY.ContractID = CNTR.ID
 	WHERE  CNTR.ID = ContractID AND 
